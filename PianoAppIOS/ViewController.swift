c81741a9e452a5b55896a67beff1fe07b73e7562
walid0010 <walidchekir98@gmail.com>
2021-07-20 22:25:40 +0100

    update

--- PianoAppIOS/ViewController.swift
+++ PianoAppIOS/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var player:AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(x: sender.currentTitle!)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2){
+            sender.alpha = 1.0
+        }
+        // ! to say we have always a title dont worry we are safe
+    }
+    
+    func playSound(x:String){
+        let url = Bundle.main.url(forResource: "Sounds/\(x)", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
+    
 }
 
